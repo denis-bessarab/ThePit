837b7e2e863f93eacf1fc1d5c4c5cd4fd79e236b
denis-bessarab <shmiterling@gmail.com>
2026-03-04 17:06:27 +0100

    04.03 dev

---
+++ Assets/Scripts/MovementSystem.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MovementSystem : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
